6f0017c7bce5335a763e61a04e477bfd68f37f0a
Sergio Alejandro Diaz Pinilla <seradiazpin@unal.edu.co>
2015-03-30 16:06:30 -0500

    Validacion de Errores

--- src/des/DES.java
+++ src/des/DES.java
@@ -5,7 +5,9 @@
  */
 package des;
 
+import java.lang.reflect.Array;
 import java.math.BigInteger;
+import java.util.Dictionary;
 import java.util.Random;
 
 /**
@@ -14,9 +16,7 @@ import java.util.Random;
  */
 public class DES {
 
-    /**
-     * @param args the command line arguments
-     */
+
     final static int[] PC1 = {57, 49, 41, 33, 25, 17, 9, 01, 58, 50, 42, 34, 26, 18, 10, 02, 59, 51, 43, 35, 27, 19, 11, 03, 60, 52, 44, 36, 63, 55, 47, 39, 31, 23, 15, 07, 62, 54, 46, 38, 30, 22, 14, 06, 61, 53, 45, 37, 29, 21, 13, 05, 28, 20, 12, 04};
     final static int[] PC2 = {14, 17, 11, 24, 01, 05, 03, 28, 15, 06, 21, 10, 23, 19, 12, 04, 26, 8, 16, 07, 27, 20, 13, 02, 41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32};
     final static int[] IP = {58, 50, 42, 34, 26, 18, 10, 02, 60, 52, 44, 36, 28, 20, 12, 04, 62, 54, 46, 38, 30, 22, 14, 06, 64, 56, 48, 40, 32, 24, 16, 8, 57, 49, 41, 33, 25, 17, 9, 01, 59, 51, 43, 35, 27, 19, 11, 03, 61, 53, 45, 37, 29, 21, 13, 05, 63, 55, 47, 39, 31, 23, 15, 07};
@@ -24,18 +24,17 @@ public class DES {
     final static int[] E = {32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8, 9, 10, 11, 12, 13, 12, 13, 14, 15, 16, 17, 16, 17, 18, 19, 20, 21, 20, 21, 22, 23, 24, 25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1};
     final static int[] P = {16,07,20,21,29,12,28,17,01,15,23,26,05,18,31,10,02,8,24,14,32,27,03,9,19,13,30,06,22,11,04,25};
     final static Sbox sBoxes = new Sbox();
-
+    
+    static String [] Stages = new String[4];
+    
+    
     public static void main(String[] args) {
         // TODO code application logic here
         String cText = Encryption("0123456789ABCDEF", "133457799BBCDFF1");
-        System.out.println("Xtext:"+cText);
+        System.out.println("Ctext:"+cText);
         System.out.println(Decryption(cText, "133457799BBCDFF1"));
         
-        
-
-
     }
-    
     public static String Encryption(String mesage,String keyS){
         byte[] textH = new BigInteger(mesage, 16).toByteArray();
         String[] key = keyGenerator(keyS);
@@ -86,18 +85,17 @@ public class DES {
         String Ci = keyP.substring(0, 28);
         String Di = keyP.substring(28, 56);
         String[] CD = new String[15];
-
-        System.out.println("C0:\t" + Ci + " D0:\t" + Di);
-
+        String ans = "";
+        ans += "C0:\t" + Ci + " D0:\t" + Di+"\n";
+                
         for (int i = 1; i <= 16; i++) {
             Ci = LSi(Ci, i);
-            System.out.print("C" + i + ":\t" + Ci);
+            ans += "C" + i + ":\t" + Ci;
             Di = LSi(Di, i);
-            System.out.println(" D" + i + ":\t" + Di);
+            ans += " D" + i + ":\t" + Di+"\n";
             keys[i - 1] = PERMUTATION(Ci + Di, PC2);
-            //System.out.println("k"+i+": "+keys[i-1]);
         }
-
+        Stages[0] = ans;
         return keys;
     }
 
@@ -110,7 +108,6 @@ public class DES {
             newK += key.charAt(permu[i] - 1);
 
         }
-
         return newK;
     }
 
@@ -131,17 +128,17 @@ public class DES {
 
         String Li = cText.substring(0, 32);
         String Ri = cText.substring(32, 64);
-        System.out.print("L0" + ":\t" + Li);
-        System.out.println(" R0" + ":\t" + Ri);
+        String ans = "";
+        ans +="L0" + ": " + Li+"\tR0" + ": " + Ri+"\n";
         String cMesage = "";
 
         for (int i = 1; i <= 16; i++) {
             String temp = Li;
             Li = Ri;
-            System.out.print("L" + i + ":\t" + Li);
-            Ri = XOR(temp, inerF(Ri, keys[i - 1]));//"00100011010010101010100110111011");
-            System.out.println(" R" + i + ":\t" + Ri);
+            Ri = XOR(temp, inerF(Ri, keys[i - 1]));
+            ans += "L" + i + ": " + Li+"\tR" + i + ": " + Ri+"\n";
         }
+        Stages[1] = ans;
         cMesage = Li + Ri;
         return cMesage;
     }
@@ -150,17 +147,17 @@ public class DES {
 
         String Li = cText.substring(0, 32);
         String Ri = cText.substring(32, 64);
-        System.out.print("L16" + ":\t" + Li);
-        System.out.println(" R16" + ":\t" + Ri);
+        String ans = "";
+        ans += "L16" + ": " + Li+"\tR16" + ": " + Ri+"\n";
         String cMesage = "";
         for (int i = 16; i >= 1; i--) {
            
             String temp = Ri;
             Ri = Li;
-            System.out.print("L" + i + ":\t" + Li);
-            Li = XOR(temp, inerF(Li, keys[i - 1]));//"00100011010010101010100110111011");
-            System.out.println(" R" + i + ":\t" + Ri);
+            Li = XOR(temp, inerF(Li, keys[i - 1]));
+            ans+="L" + i + ": " + Li+"\tR" + i + ": " + Ri+"\n";
         }
+        Stages[2] = ans;
         cMesage = Li + Ri;
         return cMesage;
     }
@@ -168,7 +165,6 @@ public class DES {
 
     public static String XORiner(String Li, String inerF) {
 
-        //System.out.println(" Le:\t"+inerF);
         BigInteger LI = new BigInteger(Li, 2);
         BigInteger INF = new BigInteger(inerF, 2);
         byte [] temp = LI.xor(INF).toByteArray();
@@ -182,71 +178,34 @@ public class DES {
     }
     public static String XOR(String Li, String inerF) {
 
-        //System.out.println(" Le:\t"+inerF);
         BigInteger LI = new BigInteger(Li, 2);
         BigInteger INF = new BigInteger(inerF, 2);
-        /*
-        byte [] temp = LI.xor(INF).toByteArray();
-        String s1 = "";
-        System.out.println("\ntemp size: "+temp.length);
-        for (int i = 0; i < temp.length; i++) {
-            s1 += String.format("%8s", Integer.toBinaryString(temp[i] & 0xFF)).replace(' ', '0');
-        }
-        */
         String res  = LI.xor(INF).toString(2);
         
         while(res.length() <  32){
             res = "0" + res;
-            //System.out.println("\nres: "+res +" ,"+res.length());
         }
         return res;
 
     }
     
-    // 011110 100001 010101 010101 011110 100001 010101 010101
-    // 011110 100001 010101 010101 011110 100001 010101 010101
-    
-    //011000 010001 011110 111010 100001 100110 010100 100111
-    //011000 010001 011110 111010 100001 100110 010100 100111
-    
-
     public static String inerF(String Ri, String ki) {
 
         Ri = expansion(Ri);
-        //System.out.println("\nRI EX: "+Ri);
         Ri = XORiner(Ri, ki);
-        //System.out.println("\nRI XOR: "+Ri);
-        //System.out.println(Ri);
-        //System.out.println("\nRi: " + Ri +",size "+ Ri.length());
         String c = "";
         String prueba = "";
-        //String t = "";
         for (int i = 0; i < 8; i++) {
 
             if (i == 7) {
-                //prueba += Ri.substring((i * 6) - 1, 47);
                 c += sBoxSearch(Ri.substring((i * 6), 48), i);
-                //t +=Ri.substring((i * 6), 48);
-                //System.out.println("LOLLLL: " + i + ":" + Ri.substring((i * 6), 48));
-                //System.out.println("c: "+c);
             } else {
-                //prueba += Ri.substring(i * 6, 6 * (i + 1));
                 c += sBoxSearch(Ri.substring(i * 6, 6 * (i + 1)), i);
-                //t+=Ri.substring(i * 6, 6 * (i + 1));
-                //System.out.println("LOLLLL: " + i + ":" + Ri.substring(i * 6, 6 * (i + 1)));
-                //System.out.println("c: "+c);
+
             }
-            //c += sBoxSearch(Ri.substring(i*6,6*(i+1)), i);
-            //System.out.println("pr"+prueba);
-            //System.out.println("R2: "+t);
-            //0101 1100 1000 0010 1011 0101 1001 0111
-            //0101 1100 1000 0010 1011 0101 1001 0111
             
         }
- 
-        //System.out.println("\nc: " + c+ c.length());
         c = permutation(c);
-        //System.out.println("\nc:\t" + c+ " ,"+c.length());
         return c;
     }
 
@@ -254,8 +213,6 @@ public class DES {
         int[][] temp = Sbox.getsBoxes(sBox);
         Integer r = Integer.parseInt(Bj.substring(0, 1) + Bj.substring(5, 6), 2);
         Integer c = Integer.parseInt(Bj.substring(1, 5), 2);
-        //System.out.println(Bj + " LOOOL2 " + Bj.substring(0, 1) + Bj.substring(5, 6) + " , " + r);
-        //System.out.println("LLOLLL3 " + Bj.substring(1, 5) + " , " + c);
         return String.format("%4s", Integer.toBinaryString(temp[r][c] & 0xFF)).replace(' ', '0');
 
     }
@@ -285,29 +242,4 @@ public class DES {
 
         return newK;
     }
-
-    public static String byteToSting(byte b) {
-
-        String s1 = String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0');
-        int charCode = Integer.parseInt(s1, 2);
-        String str = new Character((char) charCode).toString();
-        return str;
-    }
-
-    private static String swapChars(String str, int lIdx, int rIdx) {
-
-        StringBuilder sb = new StringBuilder(str);
-        char l = sb.charAt(lIdx), r = sb.charAt(rIdx);
-        sb.setCharAt(lIdx, r);
-        sb.setCharAt(rIdx, l);
-        return sb.toString();
-    }
-
-    private static String deleteChars(String str, int lIdx) {
-
-        StringBuilder sb = new StringBuilder(str);
-        sb.deleteCharAt(lIdx);
-        return sb.toString();
-    }
-
 }
